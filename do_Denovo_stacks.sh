d71bc2fae3af8ea0c4c99647d6d61abe608e174e
Kferchichi <88714988+Kferchichi@users.noreply.github.com>
2023-05-26 10:17:25 +0200

    Update do_Denovo_stacks.sh

--- do_Denovo_stacks.sh
+++ do_Denovo_stacks.sh
@@ -26,28 +26,28 @@ done
 i=1
 for sample in $samples
 do
-   ustacks -t fastq -f ./clean_data/${sample}.fastq -o ./output_stacks4/ -i $i -m 3 -M 2 -p 16 --force-diff-len 
+   ustacks -t fastq -f ./clean_data/${sample}.fastq -o ./output_stacks/ -i $i -m 3 -M 2 -p 16 --force-diff-len 
    let "i+=1";
 done
 
 #cstacks
-cstacks -n 6 -P ./output_stacks4/ -M ./popumap/popumap.txt -p 8 --k_len 15
+cstacks -n 6 -P ./output_stacks/ -M ./popumap/popumap.txt -p 8 --k_len 15
 
 #sstacks
-sstacks -P ./output_stacks4/ -M ./popumap/popumap.txt -p 8
+sstacks -P ./output_stacks/ -M ./popumap/popumap.txt -p 8
 
 
 #tsv2bam
-tsv2bam -P ./output_stacks4/ -M ./popumap/popumap.txt  -t 8
+tsv2bam -P ./output_stacks/ -M ./popumap/popumap.txt  -t 8
 
 #gstacks
-gstacks -P ./output_stack4/ -M ./popumap/popumap.txt  -t 8 
+gstacks -P ./output_stacks/ -M ./popumap/popumap.txt  -t 8 
 
 #populations
-populations -P ./output_stacks4/ -M ./popumap/popumap.txt -r 0.65 --vcf --genepop --structure --fstats --hwe -t 8
+populations -P ./output_stacks/ -M ./popumap/popumap.txt -r 0.65 --vcf --genepop --structure --fstats --hwe -t 8
 
 #vcftools
-vcftools --vcf ./output_stacks4/populations.snps.vcf  --max-missing 0.5 --maf 0.05  --recode --recode-INFO-all --out ./output_stacks1/filtered_popnovo_snps_005maf
+vcftools --vcf ./output_stacks/populations.snps.vcf  --max-missing 0.5 --maf 0.05  --recode --recode-INFO-all --out ./output_stacks1/filtered_popnovo_snps_005maf
 
 
 #populations(convert)
